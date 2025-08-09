32e986023995391ec707167d681a07e4b0282f59
MASA <128825633+masayasasdadf@users.noreply.github.com>
2025-08-09 22:43:39 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      { source: '/', destination: '/login', permanent: false },
-    ];
+    return [{ source: '/', destination: '/login', permanent: false }];
   },
 };
 module.exports = nextConfig;
